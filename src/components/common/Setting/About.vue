e845d039840214a9d3e5fb2315789238d893f16f
yveszhang <zhyf0310@163.com>
2023-05-24 22:24:18 +0800

    feat: --

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -2,7 +2,6 @@
 import { computed, onMounted, ref } from 'vue'
 import { NSpin } from 'naive-ui'
 import { fetchChatConfig } from '@/api'
-import pkg from '@/../package.json'
 import { useAuthStore } from '@/store'
 
 interface ConfigState {
@@ -41,24 +40,14 @@ onMounted(() => {
 <template>
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
-      <h2 class="text-xl font-bold">
-        Version - {{ pkg.version }}
-      </h2>
       <div class="p-2 space-y-2 rounded-md bg-neutral-100 dark:bg-neutral-700">
         <p>
-          此项目开源于
-          <a
-            class="text-blue-600 dark:text-blue-500"
-            href="https://github.com/Chanzhaoyu/chatgpt-web"
-            target="_blank"
-          >
-            GitHub
-          </a>
-          ，免费且基于 MIT 协议，没有任何形式的付费行为！
+          此项目开源于 GitHub，免费且基于 MIT 协议，没有任何形式的付费行为！
         </p>
         <p>
-          如果你觉得此项目对你有帮助，请在 GitHub 帮我点个 Star 或者给予一点赞助，谢谢！
+          如果你觉得此项目对你有帮助，希望可以长久运行，可以给予一点赞助，谢谢！
         </p>
+        <img class="w-24 h-24" src="https://web-font-1252405446.cos.ap-guangzhou.myqcloud.com/WechatIMG76.jpeg">
       </div>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI">
